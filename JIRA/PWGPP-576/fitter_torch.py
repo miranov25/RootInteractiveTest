e475995b98cc2670ed01ea2864227feba075c7fe
Marian Ivanov <majo.ivanov@gmail.com>
2020-05-13 13:34:40 +0200

    Fitter now outputs an approximation of the covariance matrix

--- JIRA/PWGPP-576/fitter_torch.py
+++ JIRA/PWGPP-576/fitter_torch.py
@@ -9,7 +9,9 @@ import torch
 import torch.nn
 import torch.optim
 
-def mse(x,y):
+import math
+
+def mse(x,y,weights,invsigma):
     """
     standard loss: mean squared error
 
@@ -17,11 +19,14 @@ def mse(x,y):
     @param y_true: target value
     @return: mse
     """
-    return torch.mean((x-y)**2)
+    return torch.sum(weights*(invsigma*(x-y)**2))
 
+def mse_matrix_sigma(x,y,weights,invsigma):
+    eps = x-y
+    v = torch.cholesky_solve(eps,invsigma)
+    return (weights*eps).dot(v)
 
-
-def curve_fit(fitfunc,x,y,params, weights = None, lossfunc = None, optimizer = torch.optim.LBFGS, ytol = 1e-5, xtol = 1e-5, max_steps = 1, optimizer_options={}):
+def curve_fit(fitfunc,x,y,params, weights = None, sigma = None, lossfunc = None, optimizer = 'lbfgs', ytol = 1e-5, xtol = 1e-5, max_steps = 20, optimizer_options={}):
     """
     curve fitting
 
@@ -32,17 +37,30 @@ def curve_fit(fitfunc,x,y,params, weights = None, lossfunc = None, optimizer = t
     @return: values of fitted parameters
     """
     
+    if optimizer == 'lbfgs':
+        optimizer = torch.optim.LBFGS
+        optimizer_options.update({'tolerance_grad':ytol,'tolerance_change':xtol,'max_iter':max_steps})
+        max_steps = 1
+    
     optimizer = optimizer(params, **optimizer_options)
         
     if lossfunc is None:
-        if weights is None:
+        if sigma is None:
             lossfunc = mse
-        elif weights.shape == y.shape:
-            lossfunc = lambda x,y:torch.sum(((x-y)/weights)**2)
+        elif sigma.shape == y.shape:
+            lossfunc = mse
         else:
-            t = weights.cholesky()
-            lossfunc = lambda x,y:torch.sum(torch.cholesky_solve((x-y).reshape([-1,1]),t)**2)
-            
+            lossfunc = mse_matrix_sigma
+    if sigma is None:
+        invsigma = 1
+    elif sigma.shape == y.shape:
+        invsigma = 1/sigma
+    else:
+        invsigma = sigma.cholesky()
+
+    if weights is None:
+        weights = 1/y.size(0)
+    
     oldparams = torch.cat([i.flatten() for i in params])
 
     #for i in range(options["max_iterations"]):
@@ -51,7 +69,7 @@ def curve_fit(fitfunc,x,y,params, weights = None, lossfunc = None, optimizer = t
         def closure():
             optimizer.zero_grad()
             y_appx = fitfunc(x,*params)
-            loss = lossfunc(y,y_appx)
+            loss = lossfunc(y,y_appx,weights,invsigma)
             loss.backward()
             return loss
         
@@ -65,14 +83,19 @@ def curve_fit(fitfunc,x,y,params, weights = None, lossfunc = None, optimizer = t
         
         stall = max(torch.max(torch.abs(newparams-oldparams)) for i in params) < xtol
         
-        oldparams = newparams.clone()
+        oldparams = newparams
         
-        if optcond or stall:
+        if optcond or stall or math.isnan(params[0].data[0].item()):
             break
    # print(i)        
+    optimizer.zero_grad()
     with torch.no_grad():
         y_fit = fitfunc(x,*params)
-        loss = lossfunc(y,y_fit)
+        loss = lossfunc(y,y_fit,weights,invsigma)
     
+    if len(params) == 1:
+        hessian = torch.autograd.functional.hessian(lambda a:lossfunc(y,fitfunc(x,a),weights,invsigma),params[0])
+    else:
+        hessian = torch.autograd.functional.hessian(lambda a:lossfunc(y,fitfunc(x,*a),weights,invsigma),tuple(params))
     
-    return params,loss,i
+    return params,hessian.detach().pinverse()*loss
